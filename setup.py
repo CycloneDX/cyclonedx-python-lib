619ee1dfc23f7220a1941c3fa5068761346c84cb
Paul Horton <phorton@sonatype.com>
2021-09-03 16:32:35 +0100

    Updated project to state support from Python v3.6+

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python :: 3'
     ],
     packages=find_packages(),
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     package_data={
         'cyclonedx': ['schema/*.json', 'schema/*.xsd', 'schema/ext/*.json', 'schema/ext/*.xsd']
     },
