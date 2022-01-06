a3f2656284f8fb7979ad0e144a1863ae637e721a
Paul Horton <phorton@sonatype.com>
2022-01-06 10:59:08 +0000

    doc: Added config for RTD.
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,18 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# encoding: utf-8
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 import pkg_resources
 
 # -- Project information -----------------------------------------------------
