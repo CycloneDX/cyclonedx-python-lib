88097bdee29a87b2414feb6104c71db325273d20
Paul Horton <paul.horton@owasp.org>
2023-01-24 13:04:02 +0000

    chore: removed unused code and minor refactor to remove duplicate code
    Signed-off-by: Paul Horton <paul.horton@owasp.org>

--- cyclonedx/schema/schema.py
+++ cyclonedx/schema/schema.py
@@ -31,90 +31,8 @@ class BaseSchemaVersion(ABC, ViewType):
     def schema_version_enum(self) -> SchemaVersion:
         pass
 
-    def bom_supports_metadata(self) -> bool:
-        return True
-
-    def bom_metadata_supports_tools(self) -> bool:
-        return True
-
-    def bom_metadata_supports_tools_external_references(self) -> bool:
-        return True
-
-    def bom_metadata_supports_licenses(self) -> bool:
-        return True
-
-    def bom_metadata_supports_properties(self) -> bool:
-        return True
-
-    def bom_supports_services(self) -> bool:
-        return True
-
-    def bom_supports_external_references(self) -> bool:
-        return True
-
-    def bom_supports_dependencies(self) -> bool:
-        return True
-
-    def services_supports_properties(self) -> bool:
-        return True
-
-    def services_supports_release_notes(self) -> bool:
-        return True
-
-    def bom_supports_vulnerabilities(self) -> bool:
-        return True
-
-    def bom_supports_vulnerabilities_via_extension(self) -> bool:
-        return False
-
-    def bom_requires_modified(self) -> bool:
-        return False
-
-    def component_supports_supplier(self) -> bool:
-        return True
-
-    def component_supports_author(self) -> bool:
-        return True
-
-    def component_supports_bom_ref_attribute(self) -> bool:
-        return True
-
-    def component_supports_mime_type_attribute(self) -> bool:
-        return True
-
-    def license_supports_expression(self) -> bool:
-        return True
-
-    def component_version_optional(self) -> bool:
-        return False
-
-    def component_supports_swid(self) -> bool:
-        return True
-
-    def component_supports_pedigree(self) -> bool:
-        return True
-
-    def pedigree_supports_patches(self) -> bool:
-        return True
-
-    def component_supports_external_references(self) -> bool:
-        return True
-
-    def component_supports_properties(self) -> bool:
-        return True
-
-    def component_supports_evidence(self) -> bool:
-        return True
-
-    def component_supports_release_notes(self) -> bool:
-        return True
-
-    def external_references_supports_hashes(self) -> bool:
-        return True
-
-    @abstractmethod
     def get_schema_version(self) -> str:
-        raise NotImplementedError
+        return self.schema_version_enum.to_version()
 
 
 class SchemaVersion1Dot4(BaseSchemaVersion):
@@ -123,39 +41,12 @@ class SchemaVersion1Dot4(BaseSchemaVersion):
     def schema_version_enum(self) -> SchemaVersion:
         return SchemaVersion.V1_4
 
-    def get_schema_version(self) -> str:
-        return '1.4'
-
-    def component_version_optional(self) -> bool:
-        return True
-
 
 class SchemaVersion1Dot3(BaseSchemaVersion):
 
     @property
     def schema_version_enum(self) -> SchemaVersion:
         return SchemaVersion.V1_3
-
-    def bom_metadata_supports_tools_external_references(self) -> bool:
-        return False
-
-    def services_supports_release_notes(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities_via_extension(self) -> bool:
-        return True
-
-    def component_supports_mime_type_attribute(self) -> bool:
-        return False
-
-    def component_supports_release_notes(self) -> bool:
-        return False
-
-    def get_schema_version(self) -> str:
-        return '1.3'
 
 
 class SchemaVersion1Dot2(BaseSchemaVersion):
@@ -164,45 +55,6 @@ class SchemaVersion1Dot2(BaseSchemaVersion):
     def schema_version_enum(self) -> SchemaVersion:
         return SchemaVersion.V1_2
 
-    def bom_metadata_supports_tools_external_references(self) -> bool:
-        return False
-
-    def bom_metadata_supports_licenses(self) -> bool:
-        return False
-
-    def bom_metadata_supports_properties(self) -> bool:
-        return False
-
-    def services_supports_properties(self) -> bool:
-        return False
-
-    def services_supports_release_notes(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities_via_extension(self) -> bool:
-        return True
-
-    def component_supports_mime_type_attribute(self) -> bool:
-        return False
-
-    def component_supports_properties(self) -> bool:
-        return False
-
-    def component_supports_evidence(self) -> bool:
-        return False
-
-    def component_supports_release_notes(self) -> bool:
-        return False
-
-    def external_references_supports_hashes(self) -> bool:
-        return False
-
-    def get_schema_version(self) -> str:
-        return '1.2'
-
 
 class SchemaVersion1Dot1(BaseSchemaVersion):
 
@@ -210,138 +62,12 @@ class SchemaVersion1Dot1(BaseSchemaVersion):
     def schema_version_enum(self) -> SchemaVersion:
         return SchemaVersion.V1_1
 
-    def bom_metadata_supports_tools(self) -> bool:
-        return False
-
-    def bom_metadata_supports_tools_external_references(self) -> bool:
-        return False
-
-    def bom_supports_services(self) -> bool:
-        return False
-
-    def services_supports_properties(self) -> bool:
-        return False
-
-    def pedigree_supports_patches(self) -> bool:
-        return False
-
-    def services_supports_release_notes(self) -> bool:
-        return False
-
-    def bom_supports_dependencies(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities_via_extension(self) -> bool:
-        return True
-
-    def bom_supports_metadata(self) -> bool:
-        return False
-
-    def component_supports_mime_type_attribute(self) -> bool:
-        return False
-
-    def component_supports_supplier(self) -> bool:
-        return False
-
-    def component_supports_author(self) -> bool:
-        return False
-
-    def component_supports_swid(self) -> bool:
-        return False
-
-    def component_supports_properties(self) -> bool:
-        return False
-
-    def component_supports_evidence(self) -> bool:
-        return False
-
-    def component_supports_release_notes(self) -> bool:
-        return False
-
-    def external_references_supports_hashes(self) -> bool:
-        return False
-
-    def get_schema_version(self) -> str:
-        return '1.1'
-
 
 class SchemaVersion1Dot0(BaseSchemaVersion):
 
     @property
     def schema_version_enum(self) -> SchemaVersion:
         return SchemaVersion.V1_0
-
-    def bom_metadata_supports_tools(self) -> bool:
-        return False
-
-    def bom_metadata_supports_tools_external_references(self) -> bool:
-        return False
-
-    def bom_supports_services(self) -> bool:
-        return False
-
-    def bom_supports_external_references(self) -> bool:
-        return False
-
-    def bom_supports_dependencies(self) -> bool:
-        return False
-
-    def services_supports_properties(self) -> bool:
-        return False
-
-    def services_supports_release_notes(self) -> bool:
-        return False
-
-    def bom_supports_vulnerabilities(self) -> bool:
-        return False
-
-    def bom_supports_metadata(self) -> bool:
-        return False
-
-    def bom_requires_modified(self) -> bool:
-        return True
-
-    def component_supports_author(self) -> bool:
-        return False
-
-    def component_supports_bom_ref_attribute(self) -> bool:
-        return False
-
-    def license_supports_expression(self) -> bool:
-        return False
-
-    def component_supports_mime_type_attribute(self) -> bool:
-        return False
-
-    def component_supports_supplier(self) -> bool:
-        return False
-
-    def component_supports_swid(self) -> bool:
-        return False
-
-    def component_supports_pedigree(self) -> bool:
-        return False
-
-    def component_supports_external_references(self) -> bool:
-        return False
-
-    def component_supports_properties(self) -> bool:
-        return False
-
-    def component_supports_evidence(self) -> bool:
-        return False
-
-    def component_supports_release_notes(self) -> bool:
-        return False
-
-    def external_references_supports_hashes(self) -> bool:
-        return False
-
-    def get_schema_version(self) -> str:
-        return '1.0'
 
 
 SCHEMA_VERSIONS = {
