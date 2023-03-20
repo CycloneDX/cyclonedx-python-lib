16870f4119865b549172cc76588ca1aa7ce00357
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-03-20 10:37:54 +0100

    chore: prevent dev-lowest-lockfile from dependency bumps (#359)
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- deps.lowest.r
+++ deps.lowest.r
@@ -6,3 +6,5 @@ py-serializable == 0.11.1
 importlib-metadata == 3.4.0 # ; python_version < '3.8'
 setuptools == 47.0.0
 types-setuptools == 57.0.0
+
+# file name is a untypical one, so dependabot does not bump this file
