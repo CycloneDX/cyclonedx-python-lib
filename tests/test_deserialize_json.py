4c2ef14017467980509bdb69f937b5098d8c0965
Jan Kowalleck <jan.kowalleck@owasp.org>
2023-10-11 11:10:04 +0200

    tests for backwards compatibility of #365 (#467)
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/test_deserialize_json.py
+++ tests/test_deserialize_json.py
@@ -17,15 +17,17 @@
 
 
 from json import loads as json_loads
+from os.path import join
 from typing import Any, Callable
 from unittest import TestCase
 from unittest.mock import patch
 
-from ddt import ddt, named_data
+from ddt import data, ddt, named_data
 
 from cyclonedx.model.bom import Bom
+from cyclonedx.model.license import DisjunctiveLicense, LicenseExpression, LicenseRepository
 from cyclonedx.schema import OutputFormat, SchemaVersion
-from tests import DeepCompareMixin, SnapshotMixin, mksname, uuid_generator
+from tests import OWN_DATA_DIRECTORY, DeepCompareMixin, SnapshotMixin, mksname, uuid_generator
 from tests._data.models import all_get_bom_funct_valid, all_get_bom_funct_with_incomplete_deps
 
 
@@ -35,7 +37,7 @@ class TestDeserializeJson(TestCase, SnapshotMixin, DeepCompareMixin):
     @named_data(*all_get_bom_funct_valid)
     @patch('cyclonedx.model.ThisTool._version', 'TESTING')
     @patch('cyclonedx.model.bom_ref.uuid4', side_effect=uuid_generator(0, version=4))
-    def test(self, get_bom: Callable[[], Bom], *_: Any, **__: Any) -> None:
+    def test_prepared(self, get_bom: Callable[[], Bom], *_: Any, **__: Any) -> None:
         # only latest schema will have all data populated in serialized form
         snapshot_name = mksname(get_bom, SchemaVersion.V1_4, OutputFormat.JSON)
         expected = get_bom()
@@ -43,3 +45,30 @@ class TestDeserializeJson(TestCase, SnapshotMixin, DeepCompareMixin):
         bom = Bom.from_json(json)
         self.assertBomDeepEqual(expected, bom,
                                 fuzzy_deps=get_bom in all_get_bom_funct_with_incomplete_deps)
+
+    @data(SchemaVersion.V1_4, SchemaVersion.V1_3, SchemaVersion.V1_2)
+    def test_mixed_licenses_before15(self, sv: SchemaVersion) -> None:
+        # before CDX 1.5 it was allowed to mix `expression` and `license`
+        def test(ls: LicenseRepository) -> None:
+            self.assertEqual(3, len(ls))
+            expression: LicenseExpression = next((
+                li for li in ls if isinstance(li, LicenseExpression)
+            ), None)
+            with_id: DisjunctiveLicense = next((
+                li for li in ls if isinstance(li, DisjunctiveLicense) and li.id is not None
+            ), None)
+            with_name: DisjunctiveLicense = next((
+                li for li in ls if isinstance(li, DisjunctiveLicense) and li.name is not None
+            ), None)
+            self.assertEqual('MIT OR Apache-2.0', expression.value)
+            self.assertEqual('MIT', with_id.id)
+            self.assertEqual('foo license', with_name.name)
+
+        json_file = join(OWN_DATA_DIRECTORY, 'json', sv.to_version(), 'bom_with_mixed_licenses.json')
+        with open(json_file) as f:
+            json = json_loads(f.read())
+        bom: Bom = Bom.from_json(json)
+        test(bom.metadata.licenses)
+        test(bom.metadata.component.licenses)
+        test(list(bom.components)[0].licenses)
+        test(list(bom.services)[0].licenses)
