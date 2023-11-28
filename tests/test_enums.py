4791e49fc8d980f5540a930e37ba72830959414a
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-11-28 18:05:04 +0100

    tests: enums
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -61,7 +61,8 @@ from cyclonedx.model.issue import (  # isort:skip
 )
 from cyclonedx.model.vulnerability import (  # isort:skip
     VulnerabilityScoreSource,  # TODO
-    VulnerabilitySeverity, BomTargetVersionRange, BomTarget, Vulnerability, VulnerabilityAnalysis,  # TODO
+    VulnerabilitySeverity, BomTargetVersionRange, BomTarget, Vulnerability, VulnerabilityAnalysis,
+    VulnerabilityRating,  # TODO
 )
 
 # endregion SUT
@@ -373,7 +374,7 @@ class TestEnumIssueClassification(_EnumTestCase):
 
     @idata(set(chain(
         dp_enum_from_xml_schemas(f"./{SCHEMA_NS}simpleType[@name='issueClassification']"),
-        dp_cases_from_json_schemas('definitions', 'issue', 'properties/type'),
+        dp_cases_from_json_schemas('definitions', 'issue', 'properties', 'type'),
     )))
     def test_knows_value(self, value: str) -> None:
         super()._test_knows_value(IssueClassification, value)
@@ -390,4 +391,24 @@ class TestEnumIssueClassification(_EnumTestCase):
                 ))
             ]))
         ])
+        super()._test_cases_render_valid(bom, of, sv)
+
+
+@ddt
+class TestEnumVulnerabilityScoreSource(_EnumTestCase):
+
+    @idata(set(chain(
+        dp_enum_from_xml_schemas(f"./{SCHEMA_NS}simpleType[@name='scoreSourceType']"),
+        dp_cases_from_json_schemas('definitions', 'scoreMethod'),
+    )))
+    def test_knows_value(self, value: str) -> None:
+        super()._test_knows_value(VulnerabilityScoreSource, value)
+
+    @named_data(*NAMED_OF_SV)
+    @patch('cyclonedx.model.ThisTool._version', 'TESTING')
+    @patch('cyclonedx.model.bom_ref.uuid4', side_effect=uuid_generator(0, version=4))
+    def test_cases_render_valid(self, of: OutputFormat, sv: SchemaVersion, *_: Any, **__: Any) -> None:
+        bom = _make_bom(vulnerabilities=[Vulnerability(bom_ref='dummy', ratings=(
+            VulnerabilityRating(method=vss) for vss in VulnerabilityScoreSource
+        ))])
         super()._test_cases_render_valid(bom, of, sv)
