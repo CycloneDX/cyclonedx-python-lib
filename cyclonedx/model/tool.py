0c700620296b81f5540c93a4d9c9a535a961313c
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-07-21 19:32:55 +0200

    chore: add legal header
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx/model/tool.py
+++ cyclonedx/model/tool.py
@@ -1,3 +1,19 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) OWASP Foundation. All Rights Reserved.
+
+
 from json import loads as json_loads
 from typing import Any, Dict, Iterable, List, Optional, Type, Union
 from warnings import warn
@@ -153,6 +169,7 @@ class ToolsRepository:
     """
     The repository of tool formats
     """
+
     def __init__(self, *, components: Optional[Iterable[Component]] = None,
                  services: Optional[Iterable[Service]] = None,
                  # Deprecated in v1.5
