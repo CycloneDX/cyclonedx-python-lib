9940cf95e619d67a2a15ff7e6784513059e6ab5e
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-09-17 10:14:05 +0200

    refactor: simplify `.builder.this.this_tool`
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx/builder/this.py
+++ cyclonedx/builder/this.py
@@ -15,9 +15,7 @@
 
 """Representation of this very python library."""
 
-__all__ = ['this_tool', 'this_component']
-
-from typing import Iterable
+__all__ = ['this_component', 'this_tool', ]
 
 from .. import __version__ as __ThisVersion  # noqa: N812
 from ..model import ExternalReference, ExternalReferenceType, XsUri
@@ -27,63 +25,9 @@ from ..model.tool import Tool
 
 # !!! keep this file in sync with `pyproject.toml`
 
-# !!!
-# things in here are built on demand, rather than using prepared frozen constants.
-# this is currently a draft and may change in the future.
-# !!!
-
-
-def __ext_refs() -> Iterable[ExternalReference]:
-    return (
-        ExternalReference(
-            type=ExternalReferenceType.BUILD_SYSTEM,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/actions')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.DISTRIBUTION,
-            url=XsUri('https://pypi.org/project/cyclonedx-python-lib/')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.DOCUMENTATION,
-            url=XsUri('https://cyclonedx-python-library.readthedocs.io/')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.ISSUE_TRACKER,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/issues')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.LICENSE,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/blob/main/LICENSE')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.RELEASE_NOTES,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/blob/main/CHANGELOG.md')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.VCS,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib')
-        ),
-        ExternalReference(
-            type=ExternalReferenceType.WEBSITE,
-            url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/#readme')
-        ),
-    )
-
-
-def this_tool() -> Tool:
-    """Representation of this very python library as a :class:`Tool`."""
-
-    return Tool(
-        vendor='CycloneDX',
-        name='cyclonedx-python-lib',
-        version=__ThisVersion or 'UNKNOWN',
-        external_references=__ext_refs(),
-    )
-
 
 def this_component() -> Component:
     """Representation of this very python library as a :class:`Component`."""
-
     return Component(
         type=ComponentType.LIBRARY,
         group='CycloneDX',
@@ -92,6 +36,44 @@ def this_component() -> Component:
         description='Python library for CycloneDX',
         licenses=(DisjunctiveLicense(id='Apache-2.0',
                                      acknowledgement=LicenseAcknowledgement.DECLARED),),
-        external_references=__ext_refs(),
-        # to be expanded ...
+        external_references=(
+            ExternalReference(
+                type=ExternalReferenceType.BUILD_SYSTEM,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/actions')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.DISTRIBUTION,
+                url=XsUri('https://pypi.org/project/cyclonedx-python-lib/')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.DOCUMENTATION,
+                url=XsUri('https://cyclonedx-python-library.readthedocs.io/')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.ISSUE_TRACKER,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/issues')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.LICENSE,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/blob/main/LICENSE')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.RELEASE_NOTES,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/blob/main/CHANGELOG.md')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.VCS,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib')
+            ),
+            ExternalReference(
+                type=ExternalReferenceType.WEBSITE,
+                url=XsUri('https://github.com/CycloneDX/cyclonedx-python-lib/#readme')
+            ),
+        ),
+        # to be extended...
     )
+
+
+def this_tool() -> Tool:
+    """Representation of this very python library as a :class:`Tool`."""
+    return Tool.from_component(this_component())
