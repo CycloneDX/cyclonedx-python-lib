1467b212ee1a7b9ea72486d6e893ce7b76c61ffe
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-09-23 14:45:43 +0200

    docs: migrate to v8.0.0
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ docs/upgrading.rst
@@ -0,0 +1,16 @@
+Upgrading to v8
+===============
+
+Version 8 is not backwards compatible. Some behaviours and integrations changed.
+This document covers all breaking changes and should give guidance how to migrate from previous versions.
+
+This document is not a full :doc:`change log <changelog>`, but a migration path.
+
+____
+
+Downstream users SHOULD add the following to their BOM build processes, to keep track of used libraries for potential troubleshooting:
+
+```py
+
+
+```
--- examples/complex_serialize.py
+++ examples/complex_serialize.py
@@ -20,6 +20,7 @@ from typing import TYPE_CHECKING
 
 from packageurl import PackageURL
 
+from cyclonedx.builder.this import this_component as cdx_lib_component
 from cyclonedx.exception import MissingOptionalDependencyException
 from cyclonedx.factory.license import LicenseFactory
 from cyclonedx.model import XsUri
@@ -43,6 +44,8 @@ lc_factory = LicenseFactory()
 # region build the BOM
 
 bom = Bom()
+bom.metadata.tools.components.add(cdx_lib_component())
+
 bom.metadata.component = root_component = Component(
     name='myApp',
     type=ComponentType.APPLICATION,
