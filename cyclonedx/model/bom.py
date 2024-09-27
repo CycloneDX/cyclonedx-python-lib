e00af1739fa6d3933315e96266d96d9b290012ee
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-09-27 12:59:56 +0200

    rename `ToolsRepository` -> `ToolRepository` (#687)
    Item class of repository is to be called in singular(`Tool`).
    
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx/model/bom.py
+++ cyclonedx/model/bom.py
@@ -44,7 +44,7 @@ from .contact import OrganizationalContact, OrganizationalEntity
 from .dependency import Dependable, Dependency
 from .license import License, LicenseExpression, LicenseRepository
 from .service import Service
-from .tool import Tool, ToolsRepository, _ToolsRepositoryHelper
+from .tool import Tool, ToolRepository, _ToolRepositoryHelper
 from .vulnerability import Vulnerability
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -62,7 +62,7 @@ class BomMetaData:
 
     def __init__(
         self, *,
-        tools: Optional[Union[Iterable[Tool], ToolsRepository]] = None,
+        tools: Optional[Union[Iterable[Tool], ToolRepository]] = None,
         authors: Optional[Iterable[OrganizationalContact]] = None,
         component: Optional[Component] = None,
         supplier: Optional[OrganizationalEntity] = None,
@@ -117,22 +117,22 @@ class BomMetaData:
     #    ... # TODO since CDX1.5
 
     @property
-    @serializable.type_mapping(_ToolsRepositoryHelper)
+    @serializable.type_mapping(_ToolRepositoryHelper)
     @serializable.xml_sequence(3)
-    def tools(self) -> ToolsRepository:
+    def tools(self) -> ToolRepository:
         """
         Tools used to create this BOM.
 
         Returns:
-            `ToolsRepository` objects.
+            :class:`ToolRepository` object.
         """
         return self._tools
 
     @tools.setter
-    def tools(self, tools: Union[Iterable[Tool], ToolsRepository]) -> None:
+    def tools(self, tools: Union[Iterable[Tool], ToolRepository]) -> None:
         self._tools = tools \
-            if isinstance(tools, ToolsRepository) \
-            else ToolsRepository(tools=tools)
+            if isinstance(tools, ToolRepository) \
+            else ToolRepository(tools=tools)
 
     @property
     @serializable.xml_array(serializable.XmlArraySerializationType.NESTED, 'author')
