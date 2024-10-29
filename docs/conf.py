543768e4b44c58026ba0da9cc3cb366a57727c28
semantic-release <semantic-release@bot.local>
2024-10-29 09:35:55 +0000

    chore(release): 8.4.0
    Automatically generated by python-semantic-release
    
    Signed-off-by: semantic-release <semantic-release@bot.local>

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Paul Horton, Jan Kowalleck, Steve Springett, Patrick Dwyer'
 
 # The full version, including alpha/beta/rc tags
 # !! version is managed by semantic_release
-release = '8.3.0'
+release = '8.4.0'
 
 # -- General configuration ---------------------------------------------------
 
