2e7d79c6e35b90760fbe38b529a04a3fea43dd1b
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-06-17 11:59:53 +0200

    feat: CryptoPrimitive cases for CycloneDX 1.7
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx/model/crypto.py
+++ cyclonedx/model/crypto.py
@@ -59,8 +59,6 @@ class CryptoAssetType(str, Enum):
 
 @serializable.serializable_enum
 class CryptoPrimitive(str, Enum):
-    # TODO: rename to `CryptoAlgorithmPrimitive`
-
     """
     This is our internal representation of the cryptoPropertiesType.algorithmProperties.primitive ENUM type within the
     CycloneDX standard.
